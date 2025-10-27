2d054484aed83d5c134706524fed38f778fb7c06
Davide Fedeli <137522657+Bestianich@users.noreply.github.com>
2025-10-27 21:23:48 +0100

    feat: IndexToGridCoord & GridCoordToIndex now static and basic logic

--- Project X/Assets/Scripts/Grid/Grid.cs
+++ Project X/Assets/Scripts/Grid/Grid.cs
@@ -14,17 +14,39 @@ public class Grid
     private Vector2Int _availableTiles;
 
 
-    public Vector2 IndexToGridCoord(int index)
+    public static Vector2 IndexToGridCoord(int index)
     {
-        int X = index % _maxSize.x;
-        int Y = Mathf.FloorToInt(index / _maxSize.x);
+        int X = index % GridManager.Instance.GetGrid()._maxSize.x;
+        int Y = Mathf.FloorToInt(index / GridManager.Instance.GetGrid()._maxSize.x);
 
         return new Vector2(X, Y);
     }
 
-    public int GridCoordToIndex(Vector2Int coord)
+    public static int GridCoordToIndex(Vector2Int coord)
     {
-        return coord.y * _maxSize.x + coord.x;
+        return coord.y * GridManager.Instance.GetGrid()._maxSize.x + coord.x;
+    }
+
+    public Vector3 GridCoordToWorldCoord(Vector2 coord)
+    {
+        //To Do
+        return Vector3.zero;
+    }
+    public AGridContent GetGridContent(int index)
+    {
+        return _content[index];
+    }
+
+    public void RemoveGridContent(int index)
+    {
+        //Salvo la reference per un futuro Destroy() o SetActive(false)
+        //AGridContent gridContent = _content[index]; 
+        _content.Remove(index);
+    }
+
+    public void AddContent(int index, AGridContent content)
+    {
+        _content.Add(index , content);
     }
 }
 
