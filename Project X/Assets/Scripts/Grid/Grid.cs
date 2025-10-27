cd48080c37faed573345f720564d1c9b1b041cb6
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-10-27 18:18:19 +0100

    created conversion functions
    from index to vector2 and vice versa

--- Project X/Assets/Scripts/Grid/Grid.cs
+++ Project X/Assets/Scripts/Grid/Grid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,5 +12,19 @@ public class Grid
     private Dictionary<int, AGridContent> _content = new Dictionary<int, AGridContent>();
     private Dictionary<int, int[]> _connections = new Dictionary<int, int[]>();
     private Vector2Int _availableTiles;
+
+
+    public Vector2 IndexToGridCoord(int index)
+    {
+        int X = index % _maxSize.x;
+        int Y = Mathf.FloorToInt(index / _maxSize.x);
+
+        return new Vector2(X, Y);
+    }
+
+    public int GridCoordToIndex(Vector2Int coord)
+    {
+        return coord.y * _maxSize.x + coord.x;
+    }
 }
 
