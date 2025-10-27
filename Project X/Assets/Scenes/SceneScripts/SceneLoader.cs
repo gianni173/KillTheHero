a89604a2b72d46845b9aa0e39cd6ac1bb0b41876
Luigi Magnanimo <diddimagnanimolm@gmail.com>
2025-10-27 22:32:09 +0100

    add play button connected with gameplayscene

--- Project X/Assets/Scenes/SceneScripts/SceneLoader.cs
+++ Project X/Assets/Scenes/SceneScripts/SceneLoader.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
     
     void Start()
     {
-        
+        LoadScene("MainScene");
     }
     
     void Update()
     {
         
     }
-    
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void PlayGame()
+    {
+        LoadScene("GameplayScene");
+    }
+
     public void QuitGame()
     {
         Debug.Log("Game Quit");
