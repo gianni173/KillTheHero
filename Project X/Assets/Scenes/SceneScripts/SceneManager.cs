dcdfef839d739dce9ea749325a10a9ab80b8525d
Luigi Magnanimo <diddimagnanimolm@gmail.com>
2025-10-27 18:22:57 +0100

    add main menu and exit button

---
+++ Project X/Assets/Scenes/SceneScripts/SceneManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+}
