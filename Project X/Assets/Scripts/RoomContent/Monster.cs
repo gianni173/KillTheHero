f1be91c55bb59fc39289af17b1c400368a1eb191
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-10-29 19:30:00 +0100

    adjusted code with entity CheckEntityTag functions

--- Project X/Assets/Scripts/RoomContent/Monster.cs
+++ Project X/Assets/Scripts/RoomContent/Monster.cs
@@ -1,36 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Monster : ARoomContentData
 {
-    private int _health;
-    private int _damage;
     private int _fameReward;
     private EntityTag[] _killTagMask;
 
     public override void Interact(Entity entity)
     {
-        foreach (EntityTag tagMask in entity.tagMask)
-        {
-            //check if any of the hero tags are found in the monsters kill tags
-            for (int i = 0; i < _killTagMask.Length; i++)
-            {
-                if (_killTagMask[i] == tagMask)
-                {
-                    Kill();
-                    return;
-                }
-            }
-
-            //check if any of the hero tags are found in the monsters interactable tags
-            for (int i = 0; i < _interactableTagMask.Length; i++)
-            {
-                if (_interactableTagMask[i] == tagMask)
-                {
-                    //add logic to kill the hero
-                    return;
-                }
-            }
-        }
+        if(entity.CheckEntityTag(_killTagMask))
+            Kill();
+        if (entity.CheckEntityTag(_interactableTagMask))
+            entity.Die();
     }
 
     public void Kill()
