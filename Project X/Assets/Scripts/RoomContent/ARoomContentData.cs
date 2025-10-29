dd4038eee910ec237367fc1ad0e9640f586fa30e
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-10-29 19:25:55 +0100

    added bool variable
    used to disable rooms'interactions

--- Project X/Assets/Scripts/RoomContent/ARoomContentData.cs
+++ Project X/Assets/Scripts/RoomContent/ARoomContentData.cs
@@ -4,6 +4,7 @@ public abstract class ARoomContentData : IEntityInteractable
     protected string _name;
     protected int _usage;
     protected EntityTag[] _interactableTagMask;
+    protected bool _isUsable;
     
     public virtual void Interact(Entity entity)
     {
