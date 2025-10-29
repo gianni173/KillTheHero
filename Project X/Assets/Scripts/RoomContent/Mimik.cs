048a3de78f07dbf2d131037381969ff4e986b07c
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-10-29 19:34:48 +0100

    added entity death

--- Project X/Assets/Scripts/RoomContent/Mimik.cs
+++ Project X/Assets/Scripts/RoomContent/Mimik.cs
@@ -5,5 +5,6 @@ public class Mimik : ARoomContentData
     public override void Interact(Entity entity)
     {
         //add logic to kill the hero and add gold to resources.
+        entity.Die();
     }
 }
