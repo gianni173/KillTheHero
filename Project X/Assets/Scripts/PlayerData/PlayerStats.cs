80e21551f8bcce6c4917754c45d9f5d493d5aa5f
Jachy Hu <ufficiohu@gmail.com>
2025-10-30 04:22:23 +0100

    feat: PlayerStats as singleton plain class

--- Project X/Assets/Scripts/PlayerData/PlayerStats.cs
+++ Project X/Assets/Scripts/PlayerData/PlayerStats.cs
@@ -1,8 +1,50 @@
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerStats
 {
-    public Resource[] Resources;
-    //public Purchasable[] PurchasableItems;
+    public static PlayerStats Instance { get; private set; } = new PlayerStats(); // Jachy Hu 30/10
+                                                                                  // now it's a plain singleton
+                                                                                  // class, in order to avoid creating
+                                                                                  // a playerStats manager
 
+    private Resource[] resources;
+    //public Purchasable[] PurchasableItems;
+    private PlayerStats()
+    {
+        initializeResources();
+    }
+
+    private void initializeResources()
+    {
+        resources = new Resource[2];
+        resources[0] = new Resource { Name = "Fame", Type = ResourceType.Fame, Quantity = 0 };
+        resources[1] = new Resource { Name = "Gold", Type = ResourceType.Gold, Quantity = 0 };
+        
+        // when is ready do the same logic for PurchasableItems
+    }
+
+    public void AddResource(ResourceType type, int quantity)
+    {
+        foreach (var resource in resources)
+        {
+            if (resource.Type == type)
+            {
+                resource.Quantity += quantity;
+                return;
+            }
+        }
+    }
+
+    public int GetResourceQuantity(ResourceType type)
+    {
+        foreach (var resource in resources)
+        {
+            if (resource.Type == type)
+            {
+                return resource.Quantity;
+            }
+        }
+        return 0;
+    }
 }
