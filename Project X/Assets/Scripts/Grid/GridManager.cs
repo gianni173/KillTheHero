c8c02d4e9f39c4d490cf09b514d6be7a3a327449
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-10-30 21:54:21 +0100

    created gizmos to see the grid

--- Project X/Assets/Scripts/Grid/GridManager.cs
+++ Project X/Assets/Scripts/Grid/GridManager.cs
@@ -33,9 +33,34 @@ public  class GridManager : MonoBehaviour
         }
     }
 
-
     public Grid GetGrid()
     {
         return _grid;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (_grid != null)
+        {
+            Vector3 cellOffset = new Vector3(_grid.WorldCellSize.x / 2, _grid.WorldCellSize.y / 2, 0);
+            Vector3 cellDimention = new Vector3(_grid.WorldCellSize.x, _grid.WorldCellSize.y, 0);
+            for (int x = 0; x < _grid.MaxSize.x; x++)
+            {
+                for (int y = 0; y < _grid.MaxSize.y; y++)
+                {
+                    Vector3 cellPos = _grid.GridCoordToWorldCoord(new Vector2(x, y));
+                    Gizmos.DrawWireCube(cellPos + cellOffset, cellDimention);
+                }
+            }
+            int[] gridIndices = _grid.GetAvailableGridIndices();
+            for(int i = 0; i <gridIndices.Length; i++)
+            {
+                Vector2 cellCoord = Grid.IndexToGridCoord(gridIndices[i]);
+                Vector3 cellPos = _grid.GridCoordToWorldCoord(cellCoord);
+                Gizmos.color = Color.black;
+                Gizmos.DrawWireCube(cellPos + cellOffset, cellDimention);
+            }
+        }
     }
 }
