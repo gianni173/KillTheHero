c94b2a693df4dc992091a824fe956388d6112388
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-10-31 18:03:16 +0100

    base static path finding function

--- Project X/Assets/Scripts/PathFinding/PathFinding.cs
+++ Project X/Assets/Scripts/PathFinding/PathFinding.cs
@@ -1,6 +1,13 @@
-using UnityEngine;
+using System.Collections.Generic;
 
 public class PathFinding
 {
-    
+    public static Path AStarPathFinding(Grid grid, int startIndex, int targetIndex)
+    {
+        // Implement A* pathfinding algorithm here
+        // This is a placeholder implementation and should be replaced with actual logic
+        List<int> path = new List<int>();
+
+        return new Path(path.ToArray(), grid);
+    }
 }
