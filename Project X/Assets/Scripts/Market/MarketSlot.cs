3e18e60099003a4ec2c75cf13d948a2f93b72706
Davide Fedeli <137522657+Bestianich@users.noreply.github.com>
2025-10-31 23:41:49 +0100

    feat: marketSlot logic with prefabs

---
+++ Project X/Assets/Scripts/Market/MarketSlot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MarketSlot : MonoBehaviour
+{
+    public Purchasable SlotData;
+    private Market _market;
+    private Button _button;
+    private Image _image;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnClick);
+        _image = GetComponent<Image>();
+        
+    }
+
+    public void Initialize(Purchasable item, Market market)
+    {
+        SlotData = item;
+        _market = market;
+        _image.sprite = SlotData.Sprite;
+    }
+    private void OnClick()
+    {
+        _market.TryPurchase(SlotData);
+    }
+
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnClick);
+    }
+}
