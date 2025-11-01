c75e6866ec2cbfbcacd76aecd1a1a977c685d918
Jachy Hu <ufficiohu@gmail.com>
2025-11-01 16:30:35 +0100

    implemented IDraggable interface

--- Project X/Assets/Scripts/Room/Room.cs
+++ Project X/Assets/Scripts/Room/Room.cs
@@ -6,4 +6,34 @@ using Sirenix.OdinInspector;
 
 public class Room : SerializedMonoBehaviour, IDraggable
 {
+    [Header("Room Configuration")]
+    [OdinSerialize]
+    private RoomData Data { get; set; }
+
+    [SerializeField] 
+    private bool _isDraggable = true;
+    public bool IsDragging { get; private set; }
+    public bool IsDraggable
+    {
+        get => _isDraggable;
+        set => _isDraggable = value;
+    }
+    #region IDraggable Implementation
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+    }
+    #endregion
 }
