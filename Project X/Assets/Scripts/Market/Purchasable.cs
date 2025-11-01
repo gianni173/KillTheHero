8cd80e9d9554e2dcc17faa512fa8d56d35c7c597
Eugenio Milazzo <ioiugi04@gmail.com>
2025-11-01 14:49:17 +0100

    Feat: Made Purchasable into an abstract SO.

--- Project X/Assets/Scripts/Market/Purchasable.cs
+++ Project X/Assets/Scripts/Market/Purchasable.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Purchasable", menuName = "Scriptable Objects/Purchasable")]
-public class Purchasable : ScriptableObject
+public abstract class Purchasable : ScriptableObject
 {
     public Sprite Sprite;
     public int MaxPurchases;
     public int Price;
     public int FameNeeded;
-
-    public void Purchase()
+    
+    public virtual void Purchase()
     {
         PlayerStats.Instance.AddResource(ResourceType.Gold , -Price);
         PlayerStats.Instance.AddPurchasableItem(this);
