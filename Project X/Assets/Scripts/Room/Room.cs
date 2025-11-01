f7f00291febdbf56b58258d058c11983417b8b6a
Jachy Hu <ufficiohu@gmail.com>
2025-11-01 16:33:11 +0100

    onPickup and onRelease events in Room

--- Project X/Assets/Scripts/Room/Room.cs
+++ Project X/Assets/Scripts/Room/Room.cs
@@ -12,6 +12,9 @@ public class Room : SerializedMonoBehaviour, IDraggable
 
     [SerializeField] 
     private bool _isDraggable = true;
+    //TODO: implementare gli eventi e logica interfacce dentro RoomDraggableSystem
+    public Action<Vector3> OnPickup;
+    public Action<Vector3> OnRelease;
     public bool IsDragging { get; private set; }
     public bool IsDraggable
     {
@@ -27,12 +30,16 @@ public class Room : SerializedMonoBehaviour, IDraggable
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
+        // pickup event invoke
+        OnPickup?.Invoke(transform.position);
     }
     public void OnDrag(PointerEventData eventData)
     {
     }
     public void OnEndDrag(PointerEventData eventData)
     {
+        // OnRelease event invoke
+        OnRelease?.Invoke(dropPosition);
     }
     }
     #endregion
