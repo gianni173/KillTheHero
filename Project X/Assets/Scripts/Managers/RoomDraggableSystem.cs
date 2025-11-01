7fe3f36e5941471fb2b8804e83f74d2e13a74ea2
Ian Cavini <iancavinipinzon@gmail.com>
2025-11-01 17:43:01 +0100

    Added a draggableUI obj and 2 functions in the RoomDraggableSystem script

--- Project X/Assets/Scripts/Managers/RoomDraggableSystem.cs
+++ Project X/Assets/Scripts/Managers/RoomDraggableSystem.cs
@@ -5,4 +5,15 @@ using UnityEngine.EventSystems;
 public class RoomDraggableSystem : MonoBehaviour
 {
     public List<IDraggable> Draggables;
+
+    public void RegisterDraggable(IDraggable iDraggable)
+    {
+
+    }//RegisterDraggable
+
+    public void UnregisterDraggable(IDraggable iDraggable)
+    {
+
+    }//UnregisterDraggable
+
 }
