229d138d86d5418419eeadf7580de641125a20bd
Jachy Hu <ufficiohu@gmail.com>
2025-11-01 16:28:47 +0100

    init: Room script

---
+++ Project X/Assets/Scripts/Room/Room.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Sirenix.Serialization;
+using Sirenix.OdinInspector;
+
+public class Room : SerializedMonoBehaviour, IDraggable
+{
+}
