7527ae8d27fa80bc1a4894287a4c6cf89638842e
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-11-03 18:04:21 +0100

    added phase change

--- Project X/Assets/Scripts/Phases/PhaseManager.cs
+++ Project X/Assets/Scripts/Phases/PhaseManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Sirenix.OdinInspector;
+using System;
 
 public class PhaseManager : Singleton<PhaseManager>
 {
@@ -7,9 +8,11 @@ public class PhaseManager : Singleton<PhaseManager>
     [SerializeField]
     private PhaseType _currentPhase;
     public PhaseType CurrentPhase => _currentPhase;
+    public Action<PhaseType> OnPhaseChanged;
     [Button]
     public void SetPhase(PhaseType newPhase)
     {
         _currentPhase = newPhase;
+        OnPhaseChanged?.Invoke(_currentPhase);
     }
 }
