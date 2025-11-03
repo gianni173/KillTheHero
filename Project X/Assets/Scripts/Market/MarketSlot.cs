a00ac27863f7a65d9cbe6ca62a730c11ffefe356
Davide Fedeli <davidefedeli8899@gmail.com>
2025-11-03 15:10:36 +0100

    updated SO and marketSlot prefab now with textField for money and fame

--- Project X/Assets/Scripts/Market/MarketSlot.cs
+++ Project X/Assets/Scripts/Market/MarketSlot.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MarketSlot : MonoBehaviour
 {
     public Purchasable SlotData;
+    [SerializeField] private TextMeshProUGUI _fameCostText;
+    [SerializeField] private TextMeshProUGUI _goldCostText;
     private Market _market; // Sarebbe meglio rendere Market un singleton
     private Button _button;
     private Image _image;
@@ -21,7 +25,10 @@ public class MarketSlot : MonoBehaviour
         SlotData = item;
         _market = market;
         _image.sprite = SlotData.Sprite;
+        _fameCostText.text = SlotData.FameNeeded.ToString();
+        _goldCostText.text = SlotData.Price.ToString();
     }
+    
     private void OnClick()
     {
         _market.TryPurchase(SlotData);
