c6f085aaafb3a92a5d151770964edc2f530daa51
Jachy Hu <ufficiohu@gmail.com>
2025-11-03 05:49:00 +0100

    added OnDragProperty in IDraggable, hid IsValidDropPosition()

--- Project X/Assets/Scripts/Interfaces/IDraggable.cs
+++ Project X/Assets/Scripts/Interfaces/IDraggable.cs
@@ -5,8 +5,11 @@ using UnityEngine.EventSystems;
 public interface IDraggable : IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Action<Vector3>  OnPickupProperty { get; set; }
+    public Action<Vector3, PointerEventData> OnDragProperty { get; set; }
+
     public Action<Vector3> OnReleaseProperty { get; set; }
+    
     public bool IsDragging { get; set; }
     public bool IsDraggable { get; set; }
-    public bool IsValidDropPosition(Vector3 position);
+    //public bool IsValidDropPosition(Vector3 position);
 }
