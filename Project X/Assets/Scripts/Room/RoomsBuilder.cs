2ce2562333a6acc9b45829cccbba239542b52c53
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-11-05 15:00:43 +0100

    changed RoomsBuilder to singleton

--- Project X/Assets/Scripts/Room/RoomsBuilder.cs
+++ Project X/Assets/Scripts/Room/RoomsBuilder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class RoomsBuilder : MonoBehaviour
+public class RoomsBuilder : Singleton<RoomsBuilder>
 {
     [Header("Prefabs to spawn")] 
     [SerializeField, AssetsOnly] 
