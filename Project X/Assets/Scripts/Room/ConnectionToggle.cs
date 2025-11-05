99bff6d2d01106fd0748c340c77654cab335c800
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-11-05 12:17:50 +0100

    created ToggleConnection Init and other methods
    CheckConnection() : bool -> is used to check if the connection exists
    UpdateVisual() -> updated the visuals connected to the ConnectionToggle

--- Project X/Assets/Scripts/Room/ConnectionToggle.cs
+++ Project X/Assets/Scripts/Room/ConnectionToggle.cs
@@ -1,17 +1,89 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ConnectionToggle : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField]
+    private Room _room;
+    public Room Room => _room;
+
+    private int _roomIndex;
+    private int _connectionIndex;
+
+    [SerializeField]
+    private GameObject _connectedVisual;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Init();
     }
 
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Clicked on ConnectionToggle");
+    }
+
+    public void Init()
+    {
+        if (_room == null)
+        {
+            Debug.LogWarning("Room or Room Data or Connections is null");
+            return;
+        }
+
+        var Grid = GridManager.Instance.Grid;
+        var roomCoord = Grid.WorldCoordToGridCoord(_room.transform.position);
+        _roomIndex = Grid.GridCoordToIndex(roomCoord);
+
+        var connectedGrid = Vector2Int.zero;
+        if (transform.localPosition.x > 0)
+        {
+            connectedGrid.x = 1;
+        }
+        else if (transform.localPosition.x < 0)
+        {
+            connectedGrid.x = -1;
+        }
+
+        if (transform.localPosition.y > 0)
+        {
+            connectedGrid.y = 1;
+        }
+        else if (transform.localPosition.y < 0)
+        {
+            connectedGrid.y = -1;
+        }
+
+        var targetCoord = roomCoord + connectedGrid;
+        _connectionIndex = Grid.GridCoordToIndex(targetCoord);
+        UpdateVisual();
+    }
+
+    public bool CheckConnection()
+    {
+        if (_room == null)
+        {
+            Debug.LogWarning("Room or Room Data or Connections is null");
+            return false;
+        }
+        var Grid = GridManager.Instance.Grid;
+        bool isConnected = Grid.Connections.ContainsKey(_roomIndex) &&
+                           Grid.Connections[_roomIndex].Contains(_connectionIndex);
+
+        // Also check if the neighbor room has a connection back to this room and that the neighbor room exists
+        bool isNeighborConnected = Grid.Connections.ContainsKey(_connectionIndex) &&
+                           Grid.Connections[_connectionIndex].Contains(_roomIndex)
+                           && Grid.GetGridContent(_connectionIndex) != null;
+
+        return isConnected && isNeighborConnected;
+    }
+    
+    public void UpdateVisual()
+    {
+        bool isConnected = CheckConnection();
+        _connectedVisual.SetActive(isConnected);
     }
 }
