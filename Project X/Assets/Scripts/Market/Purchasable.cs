996c754c9edb8e1b1c32ba1e9b7257eb6572a0e1
Davide Fedeli <davidefedeli8899@gmail.com>
2025-11-07 15:49:43 +0100

    fix PurchasableRoom

--- Project X/Assets/Scripts/Market/Purchasable.cs
+++ Project X/Assets/Scripts/Market/Purchasable.cs
@@ -7,9 +7,15 @@ public abstract class Purchasable : SerializedScriptableObject
     public int MaxPurchases;
     public int Price;
     public int FameNeeded;
-    
+
+    public virtual bool CanPurchase()
+    {
+        return true;
+    }
     public virtual void Purchase()
     {
+        if(!CanPurchase())
+            return;
         PlayerStats.Instance.AddResource(ResourceType.Gold , -Price);
         PlayerStats.Instance.AddPurchasableItem(this);
     }
