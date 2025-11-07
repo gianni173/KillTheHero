a651662f53072b45dc3b3e4d8b46aa71da22a938
Davide Fedeli <davidefedeli8899@gmail.com>
2025-11-07 15:09:59 +0100

    feat: now market show current gold and fame

--- Project X/Assets/Scripts/Market/Market.cs
+++ Project X/Assets/Scripts/Market/Market.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class Market : MonoBehaviour
@@ -6,16 +7,35 @@ public class Market : MonoBehaviour
     public Purchasable[] Items;
     public MarketSlot MarketSlotPrefab;
     public GameObject Container;
+    [SerializeField] private TextMeshProUGUI _goldText;
+    [SerializeField] private TextMeshProUGUI _fameText;
+    public static Market Instance { get; private set; }
     
     private void Awake()
     {
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+            
         foreach (Purchasable item in Items)
         {
             MarketSlot marketSlot = Instantiate(MarketSlotPrefab, Container.transform);
             marketSlot.Initialize(item , this);
         }
-        
+        UpdateTexts();
     }
+
+    public void UpdateTexts()
+    {
+        _goldText.text = "Gold: " + PlayerStats.Instance.GetResourceQuantity(ResourceType.Gold);
+        _fameText.text = "Fame: " + PlayerStats.Instance.GetResourceQuantity(ResourceType.Fame);
+    }
+    
 
     //"return false" for now, waiting until PlayerData is completed.
     public bool CanPurchase(Purchasable item)
