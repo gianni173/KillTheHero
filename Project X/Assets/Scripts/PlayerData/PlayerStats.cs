59e7666b8e8dfb61b57af94dc19da147863b3be0
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-11-07 15:32:56 +0100

    added overflow of AddResource() that accept Resource

--- Project X/Assets/Scripts/PlayerData/PlayerStats.cs
+++ Project X/Assets/Scripts/PlayerData/PlayerStats.cs
@@ -57,6 +57,19 @@ public class PlayerStats
             }
         }
     }
+    public void AddResource(Resource addedResource)
+    {
+        foreach (var resource in _resources)
+        {
+            if (resource.Type == addedResource.Type)
+            {
+                resource.Quantity += addedResource.Quantity;
+                Debug.Log($"Added {addedResource.Quantity} {addedResource.Type}. Total: {resource.Quantity}");
+                Market.Instance.UpdateTexts();
+                return;
+            }
+        }
+    }
 
     public int GetResourceQuantity(ResourceType type)
     {
